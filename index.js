f6e22f06e11664e976cebd4b6e4942bb4160e676
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-03 14:05:16 +0300

    map

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    // Split the tutorial title into an array of words
+    let words = tutorial.split(' ')
+
+    // Iterate through each word and capitalize the first character
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+
+    // Join the words back together and return the title cased string
+    return words.join(' ')
+  })
 }
+
+
+
+
+console.log(titleCased(tutorials))
